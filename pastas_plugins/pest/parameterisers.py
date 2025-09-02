c0641e10d753d9d4697361c8ad36e9f152a5ba52
Chris Nicol <chris.nicol@groundwaterlogic.com>
2025-09-02 20:56:42 +1000

    feat(PestParameteriser) StressModel PEST parameterisation classes

---
+++ pastas_plugins/pest/parameterisers.py
@@ -0,0 +1,414 @@
+from logging import getLogger
+
+import numpy as np
+import numpy.typing as npt
+from typing import Optional
+import pandas as pd
+from pathlib import Path
+from pandas import (DataFrame, Series, DatetimeIndex)
+from pastas import Model
+from pastas.stressmodels import WellModel
+from pastas.timeseries_utils import _frequency_is_supported
+from pastas_plugins.pest.solver import PestSolver
+from pypestutils.pestutilslib import PestUtilsLib
+
+pputils = PestUtilsLib() #the constructor searches for the shared lib
+logger = getLogger(__name__)
+
+__all__ = [
+    "WellModelParameteriser",
+]
+
+class BaseParameteriser:
+    """Custom PEST parameteriser base class for StressModel stresses.
+
+    Parameters
+    ----------
+    solver : pastas_plugins.pest.solver.PestSolver
+        PestSolver instance to which these parameters will apply
+    stressmodel : pastas.stressmodels.StressModel
+        StressModel for which to apply StressModel.stress rate scaling parameters to.
+    date_format : str
+        Datetime format for saving PEST model parameter input files for stressmodel. Default is \"%d/%m/%Y %H:%M:%S\".
+    
+    Attributes
+    ----------
+    modelfile : Path
+        PestSolver model input file for scaling (optimising) stressmodel stresses during PEST runs.
+    stress : DataFrame | Series
+        Stressmodel stress TimeSeries.  Series if a single stress, DataFrame if multiple (eg WellModel)
+    krig_factorfile : Path
+        Path to kriging factors file for use during PestSolver.run() calls.
+    krig_mpts : int
+        Number of kriging target points.
+    stress_pars : DataFrame
+        DataFrame of stressmodel.stress parameterisation info, as returned by solver.pf (pyemu.PestFrom).
+    stress_parcov  : pyemu.Cov
+        Prior covariance matrix for the WellModel parameters defined through this class. For use in building PEST uncertainty (.unc) files.
+    
+    Returns
+    -------
+    None
+    """
+
+    _name = "BaseParameteriser"
+    
+    def __init__(
+        self,
+        solver : PestSolver, 
+        stressmodel : StressModel,
+        date_format : Optional[str] = "%d/%m/%Y %H:%M:%S",
+        ) -> None:
+        self.solver = solver
+        self.model_ws = solver.model_ws
+        self.model = solver.model # needed?
+        self.stressmodel = stressmodel
+        self.date_format = date_format
+        self.modelfile = Path(solver.model_ws / f"{stressmodel.name}.stress_pars.csv")
+        self.stress = self.stressmodel.get_stress() # not sure Series-based stressmodels can handle squeeze argument
+        self.stress_names = self.stress.columns.to_list()
+        if isinstance(self.stressmodel, WellModel):
+            self.stress = self.stressmodel.get_stress(squeeze=False)
+        self.krig_factorfile = None
+        self.krig_mpts = None
+        self.stress_pars = None
+        self.stress_parcov = None
+        
+    @staticmethod
+    def _source_pts_minmax_range(
+        source_points : DataFrame
+        ) -> float:
+        """Internal method to calculate distance between two most distant coords in source points.
+        Parameters
+        ----------
+        source_points : DataFrame
+            DataFrame with 'x' and 'y' columns (x and y source point coordinates for kriging).
+
+        Returns
+        -------
+        float :
+            Distance between most distant coords in source points.
+        """
+        loc1 = [source_points.x.min(), source_points.y.min()]
+        loc2 = [source_points.x.max(), source_points.y.max()] 
+        return ((loc1[0] - loc2[0])**2 + (loc1[1] - loc2[1])**2)**0.5
+
+    @staticmethod
+    def _get_zones(source_points) -> int | npt.NDArray[int]:
+        """
+        Internal method to define zones array or int for kriging.
+        Zones are based on source_points if \"zone\" is in source_points.columns,
+        otherwise zones are set uniformly to 1.
+
+        Parameters
+        ----------
+        source_points : DataFrame
+            DataFrame with 'x' and 'y' columns (x and y source point coordinates for kriging).
+            
+        Returns
+        -------
+        int | npt.NDArray[int] :
+            Zone value(s) for kriging.
+        """        
+        zns = 1
+        if "zone" in ppts.columns:
+            zns = source_points.zone.astype(int).values.flatten()        
+        return zns
+        
+    def _get_ppoint_cov(
+        self,
+        source_points : DataFrame,
+        ) -> npt.NDArray[np.float64]:
+        """
+        Internal method to generate 2d kriging pilot point covariance matrix.
+
+        Parameters
+        ----------
+        source_points : DataFrame
+            DataFrame with 'x' and 'y' columns (x and y source point coordinates for kriging).
+            
+        Returns
+        -------
+        npt.NDArray[np.float64] :
+            2D matrix covmat(source_points.shape[0], npts) for source_points.
+        """
+        ppoint_cov = pputils.build_covar_matrix_2d(
+            ecs=source_points.x.values.flatten(),
+            ncs=source_points.y.values.flatten(),
+            zn=_get_zones(source_points),
+            vartype=1,
+            nugget=0.0,
+            aa=self.t_variogram_range,
+            sill=self.t_variogram_sill,
+            anis=1.0,
+            bearing=0.0,
+            ldcovmat=source_points.shape[0], #I think this is right (?). Don't think it matters as this covmat is square.
+            )
+        return ppoint_cov
+
+    def _calc_factors_2d(
+        self,
+        source_points : DataFrame,
+        target_points : DataFrame,
+        ) -> npt.NDArray[np.float64]:
+        """
+        Internal method to generate 2d kriging factors file.
+
+        Parameters
+        ----------
+        source_points : DataFrame
+            DataFrame with 'x' and 'y' columns (x and y source point coordinates for kriging).
+        target_points : DataFrame
+            DataFrame with 'x' and 'y' columns (x and y target coordinates for kriging).
+
+        Returns
+        -------
+        npt.NDArray[np.float64]
+            2D matrix covmat(source_points.shape[0], npts) for source_points.
+        """
+        #calc_kriging_factors_2d(ecs, ncs, zns, ect, nct, znt, vartype<1:spher, 2:exp, 3:gauss, 4:pow>, krigtype, aa, anis, bearing, searchrad, maxpts, minpts, factorfile, factorfiletype)
+        # # ^ first 6 vars: x, y, zone of source and target points
+        self.krig_factorfile = Path(self.model_ws / f"{self.modelfile.replace('.csv','')}.krigfactors.dat")
+        self.krig_mpts = pputils.calc_kriging_factors_2d(
+            ecs=source_points.x.values.flatten(),
+            ncs=source_points.y.values.flatten(),
+            zns=_get_zones(source_points),
+            ect=target_points.x, nct=target_points.y,
+            znt=np.ones(target_points.shape[0]).astype(int),
+            vartype=1,
+            krigtype=1,
+            aa=self.t_variogram_range,
+            anis=1.0,
+            bearing=0.0,
+            searchrad=_source_pts_minmax_range(source_points),
+            maxpts=source_points.shape[0],
+            minpts=1,
+            factorfile=self.krig_factorfile,
+            factorfiletype=0,
+            )
+        return _get_ppoint_cov(source_points)
+
+    @staticmethod
+    def _dtindex_to_days_elapsed(
+        dtindex : DatetimeIndex
+        ) -> Series[float]:
+        """
+        Internal method to convert a Pandas datetimeindex to float ndays elapsed since min datetime.
+        """
+        return dtindex.to_series().sub(dtindex.min()).total_seconds() / 86400.0
+
+    def interpolate_stresses(
+        self,
+        targval_min : Optional[float] = 0.0,
+        targval_max : Optional[float] = 1.0e+16,
+        ) -> None:
+        """
+        Class method for PestSolver.run() to interpolate stress pilot point interpolation parameters to
+        the full stress timeseries, for each stress in stressmodel.  Updates stressmodel.stress inplace with new values.
+
+        Parameters
+        ----------
+        targval_min : Optional[float]
+            Interpolation output minimum allowed value. Default is 0.0.
+        targval_max : Optional[float]
+            Interpolation output maximum allowed value. Default is 1.0e+16.
+
+        Returns
+        -------
+        None
+        """    
+        sourcevals = pd.read_csv(
+            self.modelfile,
+            index_col=0,
+            parse_dates=[0],
+            date_format=self.date_format
+            ) # one col per stress. Need to krig each for which we have adjustable parameters.
+        krig_cols = self.stress_names
+        source_stresses = self.stress.copy()
+        if sourcevals.shape[0] == 1: # constant-in-time ffill / bfill needed
+            source_stresses.loc[:, krig_cols] = np.nan
+            source_stresses.loc[source_vals.index, krig_cols] = source_vals
+            source_stresses.loc[:, krig_cols] = source_stresses.loc[:, krig_cols].ffill().bfill()
+        else: # kriging interpolation over time required
+            for krig_col in krig_cols:
+                krig_offset = 0.0
+                if source_val.min() <= 0.0:
+                    krig_offset = abs(source_val.min()) + 1.0e-3 # because we krig in log space
+                    source_val += krig_offset
+                krig_dict = pputils.krige_using_file(
+                    factorfile=self.krig_factorfile,
+                    factorfiletype=0,
+                    mpts=self.krig_mpts,
+                    krigtype=1,
+                    transtype=1,
+                    sourceval=sourceval,
+                    nointerpval: np.nan,
+                    )
+                targval = krig_dict["targval"]
+                targval -= krig_offset
+                targval[targval < targval_min] = targval_min
+                targval[targval > targval_max] = targval_max
+                source_stresses.loc[self.stress.index, krig_col] = targval
+
+        # replace stressmodel.stress -> TODO: check that this works ok
+        for stress_series in self.stressmodel.stress:
+            stress_series.series_original = source_stresses.loc[:, stress_series.name]
+            
+        
+class WellModelParameteriser(BaseParameteriser):
+        """Custom WellModel stress parameteriser.
+
+        Parameters
+        ----------
+        solver : pastas_plugins.pest.solver.PestSolver
+            PestSolver instance to which these parameters will apply
+        wellmodel : pastas.stressmodels.WellModel
+            WellModel for which to apply WellModel.stress rate parameters to.
+        stress_names : list[str] | None, optional
+            List of WellModel.stress.TimeSeries names for which stress rate parameters are to be optimised.
+            If None, all stresses in the wellmodel are parameterised. Default is None.
+        par_freq : str | None, optional
+            Frequency at which temporal WellModel rate pilot points are defined for each stress in stress_names.
+            Must be None or one of the following: (D, h, m, s, ms, us, ns) or a multiple of that e.g. "7D".
+            If None, a single (constant-in-time) stress rate parameter is defined for all stresses in stress_names.
+            Default is None.
+        t_variogram_range_freq_factor: float, optional
+            par_freq factor to define temporal variogram range to build a parameter covariance matrix for input to
+            pyemu.helpers.first_order_pearson_tikhonov(). Default is 2.0, so for example if par_freq is 365D, this means
+            pars covary up to the sill variance over 730D.
+        t_variogram_sill: float, optional
+            Temporal variogram sill (variance at t_variogram_range) used to build a scaling parameter
+            covariance matrix for input to pyemu.helpers.first_order_pearson_tikhonov(). If PestSolver.par_transform == "log", then
+            this must pertain to the log of the parameters. If par_freq is None, t_variogram_sill is used to define parameter
+            variance in the returned diagonal prior (co)variance matrix. Default is 1.0
+
+        Attributes
+        ----------
+        model_file : Path
+            Path to parameterised model input file to be used by solver.run(), with pest tpl file to be constructed using pyemu.PestFrom.add_parameters()
+        krig_factorfile : Path
+            Path to kriging factors file for use during PestSolver.run() calls.
+        stress_pars : DataFrame
+            DataFrame of stressmodel.stress parameterisation info, as returned by solver.pf (pyemu.PestFrom).            
+        stress_parcov  : pyemu.Cov
+            Prior covariance matrix for the WellModel parameters defined through this class. For use in building PEST uncertainty (.unc) files.
+        
+        Returns
+        -------
+        None
+        """
+
+        _name = "WellModelParameteriser"
+
+    def __init__(
+        self,
+        solver : PestSolver, 
+        wellmodel : WellModel,
+        stress_names : list[str] | None = None,
+        par_freq: str | None = None,
+        t_variogram_range_freq_factor: float | None = None,
+        t_variogram_sill: float = 1.0,
+        ) -> None:
+        
+        BaseParameteriser.__init__(self, solver=solver, stressmodel=wellmodel)
+
+        if stress_names is not None: # replace default all stress_names with only a selection to parameterise
+            self.stress_names = stress_names
+        if par_freq:
+            self.par_freq = _frequency_is_supported(par_freq)
+        else:
+            self.par_freq = None
+        self.t_variogram_range_freq_factor = t_variogram_range_freq_factor
+        if solver.par_transform == "log" and t_variogram_range_freq_factor is not None:
+            logger.warning(
+                "t_variogram_sill provided to PestSolver.add_well_rate_parameters() must pertain to log-transfomed \
+                parameter space (PestSolver.par_transform == \"log\""
+                )
+        self.t_variogram_sill = t_variogram_sill
+        
+    def add_stress_parameters(
+        self,
+        par_name_base : str
+        ):
+        """
+        Add WellModel pumping rate parameters for PestSolver.model.pf (pyemu.PstFrom) for each WellModel stress TimeSeries.
+        Modifies the solver.pf (pyemu.PstFrom) object in-place on calling this function.
+
+        Parameters
+        ----------
+        par_name_base : str
+            PEST parameter name base to provide to self.pf (pyemu.PstFrom). Required, because each WellModelParameteriser
+            instance should have a unique parameter base name.
+
+        Returns
+        -------
+        None
+        """
+
+        # TODO: DEFINE/HANDLE RATE PAR BOUNDS
+
+        # TODO: check par_name_base is not already in self.solver.pf - error if it is
+        
+        if self.par_freq is None:
+            # constant-in-time scaling parameter applied
+            self.stress.iloc[0,:].to_csv(self.modelfile)
+            self.stress_pars = self.solver.pf.add_parameters(
+                self.modelfile,
+                index_cols=[self.stress.index.name],
+                use_cols=self.stress_names,
+                par_type="grid",
+                par_style="direct",
+                transform=self.solver.par_transform,
+                pargp=[f"wellq.{self.stressmodel.name}{str(idx).zfill(2)}" for idx,col in enumerate(self.stress.columns)],
+                par_name_base=par_name_base,
+                # lower_bound=self.ml.parameters.loc[self.vary, "pmin"].values.tolist(),
+                # upper_bound=self.ml.parameters.loc[self.vary, "pmax"].values.tolist(),
+                # ult_lbound = self.ml.parameters.loc[self.vary, ["pmin"]].transpose().values.tolist(),
+                # ult_ubound = self.ml.parameters.loc[self.vary, ["pmax"]].transpose().values.tolist(),
+            )
+            
+            self.stress_parcov = pyemu.Cov(x=self.t_variogram_sill, names=self.stress_pars.index, isdiagonal=True)
+            
+        else:
+            # define target points for kriging by time
+            target_points = self.stress.copy()
+            target_points['x'] = BaseParameteriser._dtindex_to_days_elapsed(target_points.index)
+            target_points['y'] = 1.0
+            target_points.drop(columns=[c for c in target_points.columns if c not in ['x','y']], inplace=True)
+            # define source points for kriging
+            stress_pars = self.stress.resample(self.par_freq).first()
+            if self.stress.index.max() not in stress_pars.index: # include the last time so we don't get boundary effects in the interp
+                stress_pars.reindex(stress_pars.index.to_list() + [self.stress.index.max()])
+                stress_pars.loc[stress_pars.index] = self.stress.loc[stress_pars.index]
+            # convert stress datetime to timedelta from t0 as float(totaldays) for kriging
+            source_points = stress_pars.copy()
+            source_points['x'] = BaseParameteriser._dtindex_to_days_elapsed(stress_pars.index)
+            source_points['y'] = 1.0
+            source_points.drop(columns=[c for c in source_points.columns if c not in ['x','y']], inplace=True)
+            # define geostat variogram range for parameter interpolation (in krig_t space)
+            self.t_variogram_range = (pd.to_timedelta(self.par_freq) * self.t_variogram_range_freq_factor).total_seconds() / 86400.0
+            stress_parcov = BaseParameteriser._calc_factors_2d(
+                source_points=source_points,
+                target_points=target_points,
+                )
+            stress_pars.to_csv(self.modelfile)
+            self.stress_pars = self.solver.pf.add_parameters(
+                self.modelfile,
+                index_cols=[stress_pars.index.name],
+                use_cols=self.stress_names,
+                par_type="grid",
+                par_style="direct",
+                transform=self.solver.par_transform,
+                pargp=[f"wellq.{self.stressmodel.name}{str(idx).zfill(2)}" for idx,col in enumerate(stress_pars.columns)],
+                par_name_base=par_name_base,
+                # lower_bound=self.ml.parameters.loc[self.vary, "pmin"].values.tolist(),
+                # upper_bound=self.ml.parameters.loc[self.vary, "pmax"].values.tolist(),
+                # ult_lbound = self.ml.parameters.loc[self.vary, ["pmin"]].transpose().values.tolist(),
+                # ult_ubound = self.ml.parameters.loc[self.vary, ["pmax"]].transpose().values.tolist(),
+            )
+            self.stress_parcov = pyemu.Cov(x=stress_parcov, names=self.stress_pars.index, isdiagonal=False)
+            """# define a covariance matrix called cov using pyemu's geostatistics capabilities
+            cov = gs.covariance_matrix(df_pp.x, df_pp.y, df_pp.parnme)
+            # use the pyemu helper to construct preferred difference regularization equations 
+            # using the covariance for regularization weight
+            pyemu.helpers.first_order_pearson_tikhonov(pst, cov, reset=False)"""
